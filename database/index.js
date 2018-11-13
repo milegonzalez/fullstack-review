8286813b44259e0d2b1ecefbaf88166d4f1b8cdb
milegonzalez <mmilano1809@gmail.com>
2018-11-12 21:56:46 -0500

    database/index now saves queries to the database

--- database/index.js
+++ database/index.js
@@ -1,21 +1,49 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+// mongoose.connect('mongodb://localhost/fetcher');
+mongoose.Promise = require('bluebird');
+var promise = mongoose.connect('mongodb://localhost/repoSchema', {
+  useMongoClient: true,
+});
+
+promise.once('open', function(){
+  console.log('hey, its open!')
+})
+//do I need to requiere the database?
+// var db = require('./data/db');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
-  id: Number,
-  username: String, 
-  repos: String
-  //Content-Length: 25
+  username: String,
+  repos: [String]
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+// promise.repoSchema.insert({id: 1, username: "abc123", repos: ["a"]})
+// let obj = {'username': 'milena', 'repos': []};
+var obj = new Repo({
+  'username': 'milena',
+  'repos': ['bb']
+})
 
-}
+obj.save(function (err) {
+  if (err) console.log('err');
+})
+
+
+let save = (obj) => {
+  // // var db = connect('mongodb://localhost:1128')
+  // // TODO: Your code here
+  // // This function should save a repo or repos to
+  // // the MongoDB
+  // repoSchema.insert(obj)
+};
+
+// promise.then(function(db){
+//   Repo.insert({'username': 'milena', 'repos': ['a']}, function (err){
+
+//   });
+// });
+
 
 module.exports.save = save;
--- helpers/github.js
+++ helpers/github.js
@@ -1,26 +1,28 @@
 const request = require('request');
 const config = require('../config.js');
 
+request(options, function (err, res, body) {
+  let json = JSON.parse(body);
+  console.log(json);
+});
+
 let getReposByUsername = (username/* TODO */) => {
+GET /repos/`${username}`/`${username}`.rb
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   //added this
-  request(options, function (err, res, body) {
-    let json = JSON.parse(body);
-  });
   // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
     url: 'https://api.github.com',
     // I added this.
-    method: 'GET',
+    // method: 'GET',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
 }
 
 module.exports.getReposByUsername = getReposByUsername;
